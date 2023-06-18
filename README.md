7abb667430d873a43a04eb812c2d54cf31b2015f
jetevy <jetevy.tete@gmail.com>
2023-06-18 11:55:44 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# TypeScript MERN Amazona
+
+##Lessons
+
+1. Introduction
+2. Install Tools
+3. Create TypeScript React App by Vite
+4. Create Git Repository
---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+
+      <div>
+       <header>
+        TS Amazona
+       </header>
+       <main>
+
+       </main>
+       <footer>
+        All right reserved
+       </footer>
+      </div>
+
+  )
+}
+
+export default App
